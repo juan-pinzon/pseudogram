c2549ec1ab8a1a2c661931ac82f8c750a25932d5
Juan Carlos Pinzón <juancarlospinzon@outlook.com>
2017-04-24 00:26:51 -0500

    En el index.js se inicializa la configuración de firebase antes del renderizado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import App from './App';
 import './index.css';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDaVN5Zqf0La5C4YOw31Z2_dy9DZJ-Igk0",
+  authDomain: "pseudogram-4a109.firebaseapp.com",
+  databaseURL: "https://pseudogram-4a109.firebaseio.com",
+  projectId: "pseudogram-4a109",
+  storageBucket: "pseudogram-4a109.appspot.com",
+  messagingSenderId: "259757591308"
+})
 
 ReactDOM.render(
   <App />,
